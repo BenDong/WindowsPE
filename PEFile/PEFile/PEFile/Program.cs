30e6cf4b312ac4706fada8f8f055965b81821c3b
v-bedong <v-bedong@DEV14TESTUSE>
2017-03-21 01:19:20 -0700

    xxx

---
+++ PEFile/PEFile/PEFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PEFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"d:\_statvw.dll";
+            if (File.Exists(file))
+            {
+                PEFile peFile = new PEFile(file);
+                for (int i = 0; i < peFile.Sections.Length; i++)
+                {
+                    if (peFile.Sections[i].SectionInstance != null && peFile.Sections[i].SectionInstance.GetType().Equals(typeof(Image_Resource_Directory)))
+                    {
+                        ((Image_Resource_Directory)peFile.Sections[i].SectionInstance).ExportResource("d:\\Format", 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
